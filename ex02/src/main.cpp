03a49bc0dc7c0b3af9288e0a91d925ec2ca551a4
dacortes <dannyel.cortesr@gmail.com>
2024-04-17 15:36:37 +0200

    [ADD] identify(&p)

--- ex02/src/main.cpp
+++ ex02/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: dacortes <dacortes@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 09:22:58 by dacortes          #+#    #+#             */
-/*   Updated: 2024/04/17 10:12:00 by dacortes         ###   ########.fr       */
+/*   Updated: 2024/04/17 15:35:12 by dacortes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,22 +38,48 @@ Base	*create(void)
 	return (newClass);
 }
 
-int identify(Base *ptr)
+void identify(Base* p)
 {
 	int id = -1;
 	std::string type[3] = {"A", "B", "C"};
-	id += ((dynamic_cast<A*>(ptr) != NULL) ? 1 : 0);
-	id += ((dynamic_cast<B*>(ptr) != NULL) ? 2 : 0);
-	id += ((dynamic_cast<C*>(ptr) != NULL) ? 3 : 0);
+	id += ((dynamic_cast<A*>(p) != NULL) ? 1 : 0);
+	id += ((dynamic_cast<B*>(p) != NULL) ? 2 : 0);
+	id += ((dynamic_cast<C*>(p) != NULL) ? 3 : 0);
 	std::cout << "Indentified by reference, is: " << \
 	(id != -1 ? type[id]: "none") << std::endl;
-	return (id);
+}
+
+void identify(Base& p)
+{
+	try
+	{
+		A &a = dynamic_cast<A&>(p);
+		(void)a;
+		std::cout << "Indentified by reference, is: A" << std::endl;
+	}
+	catch(const std::exception& e){}
+	try
+	{
+		B &b = dynamic_cast<B&>(p);
+		(void)b;
+		std::cout << "Indentified by reference, is: B" << std::endl;
+	}
+	catch(const std::exception& e){}
+	try
+	{
+		C &c = dynamic_cast<C&>(p);
+		(void)c;
+		std::cout << "Indentified by reference, is: C" << std::endl;
+	}
+	catch(const std::exception& e){}
 }
 
 int main(void)
 {
 	Base *base = create();
-	if (identify(base) != -1)
+	identify(base);
+	identify(*base);
+	if (base != NULL)
 		delete base;
 	return (EXIT_SUCCESS);
 }
